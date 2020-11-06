52aa6d24f4c4f8da8667be42dbc112897f2a35aa
vitor José <vitorjsls30@gmail.com>
2020-11-05 23:05:22 -0300

    oec-01 - Adding material-ui into the solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Button } from '@material-ui/core';
 
 export const Hello = () => {
-  return(<h3>Yo! That's your first component!</h3>);
+  return(
+    <Button variant="contained" color="primary">
+      Yo! That's your first component with Material dude!
+    </Button>
+  );
 };
 
 const root = document.getElementById('root');
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000
+    port: 9000,
+    open: true
   },
   module: {
     rules: [
