213d180790febae60a131bc36e5cc6ff0318ef13
vitor José <vitorjsls30@gmail.com>
2020-11-05 21:04:42 -0300

    oec-01 - Adding webpack basic structure

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Object Edge - Address Book',
+      template: './dist/index.html'
+    })
+  ]
+}
