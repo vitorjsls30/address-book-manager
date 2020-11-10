41b4c4801126139c85f2d20b24b174d5e8a47d06
vitor José <vitorjsls30@gmail.com>
2020-11-10 00:14:15 -0300

    oec-01 - Adding menu itens and basic interactions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
@@ -17,21 +19,53 @@ const useStyles = makeStyles((theme) => ({
   title: { flexGrow: 1 }
 }));
 
+
 export const App = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [userAnchorEl, setUserAnchorEl] = useState(null);
+
+  const handleClick = (event, handlerFn) => {
+    handlerFn(event.currentTarget);
+  }
+
+  const handleClose = (handlerFn) => {
+    handlerFn(null);
+  }
+
   const classes = useStyles();
   return(
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuIcon} color="inherit">
+          <IconButton edge="start" className={classes.menuIcon} color="inherit" onClick={(e) => handleClick(e, setAnchorEl)}>
             <MenuIcon />
           </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={() => handleClose(setAnchorEl)}
+          >
+            <MenuItem onClick={() => handleClose(setAnchorEl)}>Manage Addresses</MenuItem>
+            <MenuItem onClick={() => handleClose(setAnchorEl)}>Add Address</MenuItem>
+          </Menu>
           <Typography variant="h6" className={classes.title}>
-            Address Book Manager
+            Manage Addresses
           </Typography>
-          <IconButton color="inherit">
+          <IconButton color="inherit" onClick={(e) => handleClick(e, setUserAnchorEl)}>
             <AccountCircleIcon />
           </IconButton>
+          <Menu
+            id="user-menu"
+            anchorEl={userAnchorEl}
+            keepMounted
+            open={Boolean(userAnchorEl)}
+            onClose={() => handleClose(setUserAnchorEl)}
+          >
+            <MenuItem onClick={() => handleClose(setUserAnchorEl)}>Details</MenuItem>
+            <MenuItem onClick={() => handleClose(setUserAnchorEl)}>Sign Out</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
       <div className={classes.appBarSpacer}></div>
