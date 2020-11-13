0e549802ce8ac28b96333f29de647e092a748f8a
vitor José <vitorjsls30@gmail.com>
2020-11-12 23:45:36 -0300

    oec-01 - Adjusting the AddAdress component Grid spacing

--- src/Components/AddAddress/index.js
+++ src/Components/AddAddress/index.js
@@ -5,19 +5,30 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   layout: {
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2)
+    marginLeft: theme.spacing(8),
+    marginRight: theme.spacing(8)
+  },
+  inputs: {
+    width: 200
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end'
   }
 }));
 
 export default function AddAddress() {
   const classes = useStyles();
 
-  const [cityUF, setCityUF] = useState(false);
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [zipCode, setZipCode] = useState('');
+  const [cityUF, setCityUF] = useState('');
   const [shipping, setShipping] = useState(false);
   const [billing, setBilling] = useState(false);
 
@@ -29,25 +40,40 @@ export default function AddAddress() {
     setCityUF(event.target.value);
   }
 
+  const clearData = () => {
+    setName('');
+    setAddress('');
+    setCityUF('');
+    setZipCode('');
+    setShipping(false);
+    setBilling(false);
+  }
+
+  const saveData = () => {
+  }
+
   return(
       <React.Fragment>
         <main className={ classes.layout }>
-          <Grid container direction="column" spacing={2} alignContent="center">
-            <Grid item xs={12} sm={12}>
-              <TextField id="name" label="Name" required placeholder="Type your Address name" fullWidth />
+          <Grid container spacing={2} alignContent="center">
+            <Grid item xs={12} sm={12} lg={6} xl={6}>
+              <TextField id="name" label="Name" required placeholder="Type your Address name" fullWidth 
+                value={name} onChange={(e) => setName(e.target.value)} />
             </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField id="address" label="Address" required placeholder="Type your Full Address" fullWidth />
+            <Grid item xs={12} sm={12} lg={6} xl={6}>
+              <TextField id="address" label="Address" required placeholder="Type your Full Address" fullWidth 
+                value={address} onChange={(e) => setAddress(e.target.value)} />
             </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField id="city-uf" select required label="Select the City and UF" fullWidth value={cityUF} onChange={handleChange}>
+            <Grid item xs={12} sm={6}>
+              <TextField id="city-uf" className={ classes.inputs } select required label="City and UF" value={cityUF} onChange={handleChange}>
                 <MenuItem key="rj" value="rj">RJ</MenuItem>
                 <MenuItem key="sp" value="sp">SP</MenuItem>
                 <MenuItem key="es" value="es">ES</MenuItem>
               </TextField>
             </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField id="zip-code" required label="Zip Code" placeholder="Wich ZIP code?" fullWidth/>
+            <Grid item xs={12} sm={6}>
+              <TextField id="zip-code" required label="Zip Code" placeholder="Wich ZIP code?" 
+                value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
             </Grid>
             <Grid item xs={12} sm={6}>
               <FormGroup row={true}>
@@ -57,9 +83,19 @@ export default function AddAddress() {
                 />
                 <FormControlLabel 
                   control={<Checkbox name="billing" checked={billing} onClick={(e) => handleCheck(e, setBilling)}/>}
-                  label="Billing Address"
+                  label="Billing Address?"
                 />
               </FormGroup>
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <div className={classes.buttons}>
+                <Button id="clear-btn" onClick={clearData} >
+                  Clear
+                </Button>
+                <Button id="clear-btn" onClick={saveData} color="primary" variant="contained">
+                  Save
+                </Button>
+              </div>
             </Grid>
           </Grid>
         </main>
