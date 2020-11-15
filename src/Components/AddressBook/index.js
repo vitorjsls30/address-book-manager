69dce73290daa4df58d9bef622b93a788a4a2f75
vitor José <vitorjsls30@gmail.com>
2020-11-15 12:40:31 -0300

    oec-01 - Adjusting mispelled component name

--- src/Components/AddressBook/index.js
+++ src/Components/AddressBook/index.js
@@ -4,7 +4,7 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import AddressItem from './AddressItem';
 
-export default function addressBook () {
+export default function AddressBook () {
   return(
     <Container>
       <Grid container direction="row" spacing={3}>
